1a6a31fdde91c82bba028a7fbea63d6f914feffd
carrotcold <carrotcold@gmail.com>
2020-09-14 03:59:54 +0900

    22. Thinking In JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+// React Library는 아래의 HTML을 자바스크립트 파일 안에 구문처럼 사용할 수 있게 해준다.
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// using a class component.
+// we actually get access to this thing called "state".
+class App extends Component {
+  constructor() {
+    super(); // super 키워드 이후에 this에 접근할 수 있음
+
+    this.state = {
+      string: '안녕 당근🥕'
+    };
+    // this.state를 지정할 수 있는 유일한 공간은 바로 constructor입니다.
+
+    // 이 state 내에 있는 prop들은
+    // 외부에서
+    // this.state.comment = 'Hello';
+    // 이런식으로 직접 수정X
+    // this.setState({ key: value });
+    // 외부에서는 위와같이 setState 메소드 형태를 사용해야함
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: '잘가 당근😥' })}>Change Text</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
+// React Library는 HTML을 자바스크립트 파일 안에 구문처럼 사용할 수 있게 해준다.
 import ReactDOM from 'react-dom';
+// ReactDOM은 실제로 돔과 인터액션 하는 로봇이고
+// 얘는 휴대폰이나 데스트탑앱과 소통하는 로봇으로 대체될 수 있다.
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
